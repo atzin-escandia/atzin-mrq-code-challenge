120fdddc8de81924573550647f5477922091bdbf
Atzin Escandia <escandiatech@gmail.com>
2025-01-21 13:45:52 +0100

    feat(styles): (SymbolCard) Add styles and restructure cards layout

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -1,7 +1,9 @@
-import './symbolCard.css';
 import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
+import { ReactComponent as MarketCapIcon } from '@/assets/market_cap.svg';
+import { ReactComponent as IndustryIcon } from '@/assets/industry.svg';
 import { useAppSelector } from '@/hooks/redux';
 import ListItem from '@/components/ListItem';
+import './SymbolCard.css';
 
 type SymbolCardProps = {
   id: string;
@@ -10,18 +12,26 @@ type SymbolCardProps = {
 };
 
 const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
-  const { trend, companyName } = useAppSelector((state) => state.stocks.entities[id]);
+  const { trend, companyName, industry, marketCap } = useAppSelector((state) => state.stocks.entities[id]);
+  const marketCapString = marketCap.toString();
   const handleOnClick = () => {
     onClick(id);
   };
+
   return (
     <div onClick={handleOnClick} className="symbolCard">
-      <div>
+      <div className="symbolCard__header">
         {id} - {trend}
       </div>
-      <div>Price:</div>
-      <div>{price || '--'} </div>
-      <ListItem Icon={<CompanyIcon />} label={companyName} />
+      <div className="symbolCard__content">
+        <div className="symbolCard__price">
+          <div>Price:</div>
+          <div>{price || '--'} </div>
+        </div>
+        <ListItem Icon={<CompanyIcon />} label={companyName} spacing="space-between" />
+        <ListItem Icon={<IndustryIcon />} label={industry} spacing="space-between" />
+        <ListItem Icon={<MarketCapIcon />} label={marketCapString} spacing="space-between" />
+      </div>
     </div>
   );
 };
