ff4d6eca902065c1e26ec91bef2d56b76540333e
Atzin Escandia <escandiatech@gmail.com>
2025-01-21 18:27:30 +0100

    feat(components): (SymbolCard) Create styles for card glow up-down - Create hook useGlowEffect to add glow effect

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -8,6 +8,7 @@ import SymbolPriceFormatter from '../SymbolPriceFormatter';
 import { priceFormatter } from '@/lib';
 import SymbolCardHeader from '../SymbolCardHeader';
 import { usePriceVariation } from '@/hooks/usePriceVariation';
+import { useGlowClass } from '@/hooks/useGlowEffect';
 
 type SymbolCardProps = {
   id: string;
@@ -24,19 +25,22 @@ const SymbolCard = ({ id, onClick, price, symbolId }: SymbolCardProps) => {
   const isSelected = symbolId === id;
   const isUnselected = symbolId && symbolId !== id;
   const { hasBigVariation } = usePriceVariation(price);
+  const glowClass = useGlowClass(price);
 
   const handleOnClick = () => {
     onClick(id);
   };
 
+  const classesSymbolCard = `
+    ${glowClass}
+        ${isSelected ? 'symbolCard_selected' : ''} 
+        ${isUnselected ? 'symbolCard_unselected' : ''}
+        ${hasBigVariation ? 'symbolCard__shake' : ''}`
+
   return (
     <div
       onClick={handleOnClick}
-      className={`symbolCard 
-        ${isSelected ? 'symbolCard_selected' : ''} 
-        ${isUnselected ? 'symbolCard_unselected' : ''}
-        ${hasBigVariation ? 'symbolCard__shake' : ''}
-        `}
+      className={`symbolCard ${classesSymbolCard}`}
     >
       <SymbolCardHeader trend={trend} id={id} />
       <div className="symbolCard__content">
